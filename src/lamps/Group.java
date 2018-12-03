be4ffa3286256624f83282da1daed0c1a3e76fd2
Jartreg <jartreg@users.noreply.github.com>
2018-12-03 14:12:04 +0100

    Add comments

--- src/lamps/Group.java
+++ src/lamps/Group.java
@@ -2,6 +2,9 @@ package lamps;
 
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Eine Gruppe von Lampen
+ */
 public class Group {
     private SimpleStringProperty name;
 
@@ -9,14 +12,26 @@ public class Group {
         this.name = new SimpleStringProperty(name);
     }
 
+    /**
+     * Gibt den Namen der Gruppe zurück
+     * @return  der Name der Gruppe
+     */
     public String getName() {
         return name.get();
     }
 
+    /**
+     * Der Name der Gruppe
+     * @return die Namenseigenschaft
+     */
     public SimpleStringProperty nameProperty() {
         return name;
     }
 
+    /**
+     * Setzt den Namen der Gruppe
+     * @param name der neue Name der Gruppe
+     */
     public void setName(String name) {
         this.name.set(name);
     }
--- src/lamps/GroupNameStringConverter.java
+++ src/lamps/GroupNameStringConverter.java
@@ -3,10 +3,14 @@ package lamps;
 import javafx.scene.control.ListCell;
 import javafx.util.StringConverter;
 
+/**
+ * Konvertiert {@link Group}s zu {@link String}s und umgekehrt.
+ * Diese Klasse wird benötigt, um den Namen einer Gruppe in der Liste zu ändern.
+ */
 class GroupNameStringConverter extends StringConverter<Group> {
     private final ListCell<Group> cell;
 
-    public GroupNameStringConverter(ListCell<Group> cell) {
+    GroupNameStringConverter(ListCell<Group> cell) {
         this.cell = cell;
     }
 
